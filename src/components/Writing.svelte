1334c6c147946a252b7267b97719ca9d22735429
spennyMain <26611804+spennymain@users.noreply.github.com>
2022-02-27 18:58:23 -0500

    adds writing with fly in

--- src/components/Writing.svelte
+++ src/components/Writing.svelte
@@ -1,25 +1,36 @@
 <script>
   import { fly } from 'svelte/transition';
-  let visible = false;
+  let visible_ukr = false;
+  let visible_canada = false;
+
 </script>
 
 <div>
   <h2>a collection of writings - accumulated over time.</h2> 
   <div class="buttons">
-    <button on:click={() => visible = !visible}>on ukraine</button>
+    <button on:click={() => visible_ukr = !visible_ukr}>on ukraine</button>
+    <button on:click={() => visible_canada = !visible_canada}>on canadian democracy</button>
   </div>
-  {#if visible}
-  <p transition:fly="{{ y: 200, duration: 2000 }}">
-  incoming content
-  </p>
+  {#if visible_ukr}
+  <div transition:fly="{{ y: 200, duration: 2000 }}">
+    <h3>many things have changed since d-0</h3>
+    <p>as we approach d-5 there exists hope, faith for the good, contrasted by loss, destruction.</p>
+  </div>
+  {/if}
+  {#if visible_canada}
+  <div transition:fly="{{ y: 200, duration: 2000 }}">
+    <h3>the importance of honest leadership and the weakness of democracy</h3>
+    <p>I find myself, and others, thinking of the government as an entity of control in constant conflict against the will of the people. But democracy <i>is</i> the people, there is no other way about it.</p>
+  </div>
   {/if}
 </div>
 
 <style>
-  h2, p {
+  h2, div{
     text-align: center;
   }
   .buttons button {
     padding: 0.25% 5%;
+    font-size: large;
   }
 </style>
